a9ed6dc673e5344a2ac49356d4862f962aa37791
Rishi Raj Jain <rishirajjain319@gmail.com>
2020-11-20 21:24:48 +0530

    initial build

---
+++ components/spicelist.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nuxt-link :to="'/spices/' + id">
+      <div class="flex items-center py-6">
+        <div class="ml-4">
+          <div class="text-lg font-semibold text-gray-900">
+            {{ name }}
+          </div>
+          <div class="text-sm text-gray-500">
+            {{ specs }}
+          </div>
+        </div>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "specs", "id"],
+};
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template >
+  <div
+    class="content-wrapper bg-gray-200 bg-no-repeat bg-right-top"
+    style="background-image: url(/illus.svg)"
+  >
+    <Nav />
+    <Nuxt />
+  </div>
+</template>
+<script>
+import Nav from "@/components/Nav.vue";
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
